12c61949f8f8524113765aa25134bd2a60f346b1
Juan David <juandavid0306@hotmail.com>
2021-02-09 11:44:08 -0500

    (New) Add bubble_sort method

--- app.rb
+++ app.rb
@@ -1,1 +1,27 @@
+def bubble_sort(array)
+  limit = array.length
 
+  while limit > 0 do
+    i = 0
+    while i < limit - 1 do
+      first = i
+      second = i + 1
+
+      if array[first] > array[second]
+        first_element = array[first]
+        second_element = array[second]
+        array[first] = second_element
+        array[second] = first_element
+      end
+
+      i += 1
+    end
+
+    limit -= 1
+  end
+
+  array
+end
+
+p bubble_sort [5, 4, 3, 2, 1]
+p bubble_sort [5, 4, 19, 2, 1, 22]
